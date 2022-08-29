c42a4be64924f7b54fbadc78a661760f7c8b5454
cheat2001 <chansocheatsok2001@gmail.com>
2022-08-29 11:50:32 +0700

    edit response

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -15,18 +15,18 @@ function SideBar() {
    }
 
   return (
-    <div className='top-0 z-10 border-r-[0.5px] border-[#4d7c62] shadow-lg fixed w-[70px]  h-[100vh] bg-[#173e43] flex flex-col items-center pt-[70px]'>
-      <NavLink style={navLinkStyle} to="/"  className="w-[100%] p-5 hover:bg-purple-500 duration-300">
-        <img src={Home} alt="" className='w-[50px]'/>
+    <div className='top-0 z-10 border-r-[0.5px] border-[#4d7c62] shadow-lg fixed w-[50px] sm:w-[70px]  h-[100vh] bg-[#173e43] flex flex-col items-center pt-[70px]'>
+      <NavLink style={navLinkStyle} to="/"  className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+        <img src={Home} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/education"  className="w-[100%] p-5 hover:bg-purple-500 duration-300">
-        <img src={Education} alt="" className='w-[50px]'/>
+      <NavLink style={navLinkStyle} to="/education"  className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+        <img src={Education} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/entertain" className="w-[100%] p-5 hover:bg-purple-500 duration-300">
-        <img src={entertain} alt="" className='w-[50px]'/>
+      <NavLink style={navLinkStyle} to="/entertain" className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+        <img src={entertain} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/news" className="w-[100%] p-5 hover:bg-purple-500 duration-300">
-        <img src={newspaper} alt="" className='w-[50px]'/>
+      <NavLink style={navLinkStyle} to="/news" className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+        <img src={newspaper} alt="" className='min-w-[30px]'/>
       </NavLink>
     </div>
   )
