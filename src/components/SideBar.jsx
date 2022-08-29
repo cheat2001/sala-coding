b9a3f8356ce1a8703915565d9395fa124b5f9d59
cheat2001 <chansocheatsok2001@gmail.com>
2022-08-29 14:13:27 +0700

    change mobile respone

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,22 +10,22 @@ function SideBar() {
     return{
         backgroundColor:isActive?"#3fb0ac":"transparent",
         transition:isActive?"1s":"1s",
-        borderRadius:isActive?"25%":"25%",
+        borderRadius:isActive?"15%":"15%",
     }
    }
 
   return (
     <div className='top-0 z-10 border-r-[0.5px] border-[#4d7c62] shadow-lg fixed w-[50px] sm:w-[70px]  h-[100vh] bg-[#173e43] flex flex-col items-center pt-[70px]'>
-      <NavLink style={navLinkStyle} to="/"  className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+      <NavLink style={navLinkStyle} to="/"  className="w-[100%] p-3 sm:p-5  duration-300 flex justify-center items-center">
         <img src={Home} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/education"  className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+      <NavLink style={navLinkStyle} to="/education"  className="w-[100%] p-3 sm:p-5 duration-300 flex justify-center items-center">
         <img src={Education} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/entertain" className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+      <NavLink style={navLinkStyle} to="/entertain" className="w-[100%] p-3 sm:p-5  duration-300 flex justify-center items-center">
         <img src={entertain} alt="" className='min-w-[30px]'/>
       </NavLink>
-      <NavLink style={navLinkStyle} to="/news" className="w-[100%] p-3 sm:p-5 hover:bg-purple-500 duration-300 flex justify-center items-center">
+      <NavLink style={navLinkStyle} to="/news" className="w-[100%] p-3 sm:p-5  duration-300 flex justify-center items-center">
         <img src={newspaper} alt="" className='min-w-[30px]'/>
       </NavLink>
     </div>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,15 +3,15 @@ import ReactPlayer from 'react-player'
 import LucaVideo from "../video/luca.mp4";
 function VideoPlayer() {
     return (
-    <div className='w-[100%] min-h-[92.4vh] overflow-hidden bg-[#0D0D0D]  absolute left-[0] p-6 pl-[100px] top-[70px]'>
+    <div className=' w-[100%] min-h-[90vh] overflow-hidden  left-[0]  p-[5px] sm:p-6 sm:pl-[100px] pl-[60px] top-[70px]'>
         <div className='w-[100%] h-[50vh] sm:h-[70vh]'>
-        <ReactPlayer  controls={true} url={LucaVideo}      width='100%'
+        <ReactPlayer className="shadow-9xl"  controls={true} url={LucaVideo}  width='100%'
           height='100%'  />
         </div>
-        <div className='text-white mt-[50px]'>
+        <div className='text-white mt-[50px] p-[10px]'>
         <div className='w-[100%] h-[1px] bg-[#ffffff88]'></div>
-       <h1 style={{fontSize:"30px"}}>Untited video trailer</h1>
-        <p style={{fontSize:"25px"}}>2022</p>
+       <h1 className='text-xl sm:text-3xl'>Untited video trailer</h1>
+        <p className='sm:text-xl'>2022</p>
        </div>
     </div>
   )
